1e06d2ac76a4e451242423fd162b8c47f0caa069
Daniel Lucraft <dan@fluentradical.com>
2010-01-22 22:40:23 +0000

    Updated the README and added comments to the first example

--- ex1_button/ex1_button.rb
+++ ex1_button/ex1_button.rb
@@ -5,22 +5,41 @@ require 'swt/swt_wrapper'
 class ButtonExample
 
   def initialize
-    @shell = Swt::Widgets::Shell.new(Swt::Widgets::Display.get_current)
-    @shell.setSize(450, 200)
+    # A Display is the connection between SWT and the native GUI. (jruby-swt-cookbook/apidocs/org/eclipse/swt/widgets/Display.html)
+    display = Swt::Widgets::Display.get_current
+  
+    # A Shell is a window in SWT parlance. (jruby-swt-cookbook/apidocs/org/eclipse/swt/widgets/Shell.html)
+    @shell = Swt::Widgets::Shell.new
+    
+    # A Shell must have a layout. FillLayout is the simplest.
     layout = Swt::Layout::FillLayout.new
-    @shell.setLayout layout
+    @shell.setLayout(layout)
 
+    # Create a button widget (jruby-swt-cookbook/apidocs/org/eclipse/swt/widgets/Button.html)
     button = Swt::Widgets::Button.new(@shell, Swt::SWT::PUSH)
     button.setText("Click Me")
-  		
+    
+    # Add a button click listener
+    button.add_selection_listener do
+      puts "You clicked the button."
+    end
+    
+    # This lays out the widgets in the Shell
     @shell.pack
+    
+    # And this displays the Shell
     @shell.open
   end
   
+  # This is the main gui event loop
   def start
     display = Swt::Widgets::Display.get_current
+    
+    # until the window (the Shell) has been closed
     while !@shell.isDisposed
-      display.sleep unless display.readAndDispatch
+    
+      # check for and dispatch new gui events
+      display.sleep unless display.read_and_dispatch
     end
 
     display.dispose
